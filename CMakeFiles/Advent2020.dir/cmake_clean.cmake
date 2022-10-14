0fd036ee35113d5bb33abbe59ef01105cdf49af2
Bryan Covell <bpc6@vt.edu>
2022-10-14 14:25:40 -0400

    using tutorial to build the project

---
+++ CMakeFiles/Advent2020.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Advent2020.dir/src/main.cpp.o"
+  "bin/Advent2020"
+  "bin/Advent2020.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Advent2020.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/Advent-2020.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "../bin/Advent-2020"
+  "../bin/Advent-2020.pdb"
+  "CMakeFiles/Advent-2020.dir/src/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Advent-2020.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
